82e386fba03e370007a4c35b5e0564b9ad6c65e1
Nirav <niravbeni11@gmail.com>
2026-01-26 15:26:07 +0000

    Clean up unused files and add Vercel config for deployment

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Scene } from './Scene'
-export { Letter, LetterPlaceholder } from './Letter'
-export { FloatingStars, FloatingStarsFallback } from './FloatingStars'
-export { JarBoundaries } from './JarBoundaries'
-
-
